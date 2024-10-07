ffc5b23130d9cd68f82ed5a6def7bd3a31248ed6
Xavier ROGNON <xavier.rognon@epitech.eu>
2024-10-07 18:29:19 +0200

    [Feature] [+] = adding test for the ACKMissing

--- tests/protocolHandler/decoder_test.cpp
+++ tests/protocolHandler/decoder_test.cpp
@@ -9,6 +9,7 @@
 
 #include "../../src/protocolHandler/Decoder.hh"
 #include <criterion/internal/assert.h>
+#include <cstdint>
 
 Test(main, TestHeaderDecoder) {
     U_STRING packetHeader = RType::Encoder::header(23, 70, RType::CONNEXION);
@@ -31,68 +32,68 @@ Test(main, TestNewEntityPacketDecoder) {
     U_STRING packet = RType::Encoder::newEntity(12, 1, 65000, 3, 4);
     COMMAND_ARGS args = RType::Decoder::newEntity(packet);
 
-    cr_assert_eq(args[0], 1, "Expected: 1, Got: %lu", args[0]);
-    cr_assert_eq(args[1], 65000, "Expected: 65000, Got: %lu", args[1]);
-    cr_assert_eq(args[2], 3, "Expected: 3, Got: %lu", args[2]);
-    cr_assert_eq(args[3], 4, "Expected: 4, Got: %lu", args[3]);
+    cr_assert_eq(args[0], 1, "Expected: 1, Got: %d", args[0]);
+    cr_assert_eq(args[1], 65000, "Expected: 65000, Got: %d", args[1]);
+    cr_assert_eq(args[2], 3, "Expected: 3, Got: %d", args[2]);
+    cr_assert_eq(args[3], 4, "Expected: 4, Got: %d", args[3]);
 }
 
 Test(main, TestDeleteEntityPacketDecoder) {
     U_STRING packet = RType::Encoder::deleteEntity(12, 1);
     COMMAND_ARGS args = RType::Decoder::deleteEntity(packet);
 
-    cr_assert_eq(args[0], 1, "Expected: 1, Got: %lu", args[0]);
+    cr_assert_eq(args[0], 1, "Expected: 1, Got: %d", args[0]);
 }
 
 Test(main, TestMoveEntityPacketDecoder) {
     U_STRING packet = RType::Encoder::moveEntity(12, 1, 2, 3, 4);
     COMMAND_ARGS args = RType::Decoder::moveEntity(packet);
 
-    cr_assert_eq(args[0], 1, "Expected: 1, Got: %lu", args[0]);
-    cr_assert_eq(args[1], 2, "Expected: 2, Got: %lu", args[1]);
-    cr_assert_eq(args[2], 3, "Expected: 3, Got: %lu", args[2]);
-    cr_assert_eq(args[3], 4, "Expected: 4, Got: %lu", args[3]);
+    cr_assert_eq(args[0], 1, "Expected: 1, Got: %d", args[0]);
+    cr_assert_eq(args[1], 2, "Expected: 2, Got: %d", args[1]);
+    cr_assert_eq(args[2], 3, "Expected: 3, Got: %d", args[2]);
+    cr_assert_eq(args[3], 4, "Expected: 4, Got: %d", args[3]);
 }
 
 Test(main, TestInfoLevelPacketDecoder) {
     U_STRING packet = RType::Encoder::infoLevel(12, 17);
     COMMAND_ARGS args = RType::Decoder::infoLevel(packet);
 
-    cr_assert_eq(args[0], 17, "Expected: 17, Got: %lu", args[0]);
+    cr_assert_eq(args[0], 17, "Expected: 17, Got: %d", args[0]);
 }
 
 Test(main, TestInfoEntityPacketDecoder) {
     U_STRING packet = RType::Encoder::infoEntity(12, 2, 23, 345, 12, 0, 123);
     COMMAND_ARGS args = RType::Decoder::infoEntity(packet);
 
-    cr_assert_eq(args[0], 2, "Expected: 2, Got: %lu", args[0]);
-    cr_assert_eq(args[1], 23, "Expected: 23, Got: %lu", args[1]);
-    cr_assert_eq(args[2], 345, "Expected: 345, Got: %lu", args[2]);
-    cr_assert_eq(args[3], 12, "Expected: 12, Got: %lu", args[3]);
-    cr_assert_eq(args[4], 0, "Expected: 0, Got: %lu", args[4]);
-    cr_assert_eq(args[5], 123, "Expected: 123, Got: %lu", args[5]);
+    cr_assert_eq(args[0], 2, "Expected: 2, Got: %d", args[0]);
+    cr_assert_eq(args[1], 23, "Expected: 23, Got: %d", args[1]);
+    cr_assert_eq(args[2], 345, "Expected: 345, Got: %d", args[2]);
+    cr_assert_eq(args[3], 12, "Expected: 12, Got: %d", args[3]);
+    cr_assert_eq(args[4], 0, "Expected: 0, Got: %d", args[4]);
+    cr_assert_eq(args[5], 123, "Expected: 123, Got: %d", args[5]);
 }
 
 Test(main, TestMovePlayerPacketDecoder) {
     U_STRING packet = RType::Encoder::movePlayer(12, 45, 12);
     COMMAND_ARGS args = RType::Decoder::movePlayer(packet);
 
-    cr_assert_eq(args[0], 45, "Expected: 45, Got: %lu", args[0]);
-    cr_assert_eq(args[1], 12, "Expected: 12, Got: %lu", args[1]);
+    cr_assert_eq(args[0], 45, "Expected: 45, Got: %d", args[0]);
+    cr_assert_eq(args[1], 12, "Expected: 12, Got: %d", args[1]);
     packet = RType::Encoder::movePlayer(12, -100, -100);
     args = RType::Decoder::movePlayer(packet);
-    cr_assert_eq(args[0], -100, "Expected: -100, Got: %ld", args[0]);
-    cr_assert_eq(args[1], -100, "Expected: -100, Got: %ld", args[1]);
+    cr_assert_eq(args[0], -100, "Expected: -100, Got: %d", args[0]);
+    cr_assert_eq(args[1], -100, "Expected: -100, Got: %d", args[1]);
 }
 
 Test(main, TestActionPlayerPacketDecoder) {
     U_STRING packet = RType::Encoder::actionPlayer(12, false, true, false, true);
     COMMAND_ARGS args = RType::Decoder::actionPlayer(packet);
 
-    cr_assert_eq(args[0], 0, "Expected: 0, Got: %lu", args[0]);
-    cr_assert_eq(args[1], 1, "Expected: 1, Got: %lu", args[1]);
-    cr_assert_eq(args[2], 0, "Expected: 0, Got: %lu", args[2]);
-    cr_assert_eq(args[3], 1, "Expected: 1, Got: %lu", args[3]);
+    cr_assert_eq(args[0], 0, "Expected: 0, Got: %d", args[0]);
+    cr_assert_eq(args[1], 1, "Expected: 1, Got: %d", args[1]);
+    cr_assert_eq(args[2], 0, "Expected: 0, Got: %d", args[2]);
+    cr_assert_eq(args[3], 1, "Expected: 1, Got: %d", args[3]);
 }
 
 Test(main, TestGameStartPacketDecoder) {
@@ -109,4 +110,15 @@ Test(main, TestGameEndPacketDecoder) {
     cr_assert_eq(RType::Decoder::getType(packetHeader), RType::GAME_END);
     cr_assert_eq(RType::Decoder::getSize(packetHeader), 70, "Expected: 70, Got: %lu",
                  RType::Decoder::getSize(packetHeader));
+}
+
+Test(main, TestACKMissing) {
+    std::vector<std::uint8_t> packets = {0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59};
+    U_STRING packetHeader = RType::Encoder::ACKMissing(12, packets);
+    COMMAND_ARGS args = RType::Decoder::ACKMissing(packetHeader);
+
+    cr_assert_eq(args.size(), packets.size(), "Size Expected: %lu, Got: %lu", packets.size(), args.size());
+    for (std::size_t i = 0; i < packets.size(); i++) {
+        cr_assert_eq(args[i], packets[i], "Expected at index %lu : %d, Got: %d", i, packets[i], args[i]);
+    }
 }
