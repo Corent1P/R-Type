fa30d4a53a6b7bc402fe7f76a845838190a0c1a6
RAMSTEIN Sacha <sacha.ramstein@epitech.eu>
2024-10-08 17:46:18 +0200

    [Feature] [+] = Intersect movement is homemade and not use sfml function File Modified : src/ecs/Systems/HandleColisionSystem.cpp

--- src/ecs/Systems/HandleColisionSystem.cpp
+++ src/ecs/Systems/HandleColisionSystem.cpp
@@ -19,11 +19,21 @@ RType::HandleColisionSystem::~HandleColisionSystem()
 void RType::HandleColisionSystem::effects(std::vector<std::shared_ptr<RType::Entity>> entities) {
     for (const auto &entity: entities) {
         if (verifyRequiredComponent(entity)) {
-            auto playerGlobalBounds = entity->getComponent<SpriteComponent>()->getSprite()->getGlobalBounds();
+            auto position1 = entity->getComponent<PositionComponent>()->getPositions();
+            auto width1 = entity->getComponent<IntRectComponent>()->getIntRectWidth();
+            auto height1 = entity->getComponent<IntRectComponent>()->getIntRectWidth();
             for (const auto &otherEntity: entities)
-                if (entity->getId() != otherEntity->getId() && otherEntity->getComponent<RType::SpriteComponent>() != nullptr && otherEntity->getComponent<RType::EntityTypeComponent>()->getEntityType() != RType::LAYER) {
-                    auto otherGlobalBounds = otherEntity->getComponent<SpriteComponent>()->getSprite()->getGlobalBounds();
-                    if (playerGlobalBounds.intersects(otherGlobalBounds)) {
+                if (entity->getId() != otherEntity->getId() && otherEntity->getComponent<RType::PositionComponent>() != nullptr && otherEntity->getComponent<RType::EntityTypeComponent>()->getEntityType() != RType::LAYER) {
+                    auto position2 = otherEntity->getComponent<PositionComponent>()->getPositions();
+                    auto width2 = otherEntity->getComponent<IntRectComponent>()->getIntRectWidth();
+                    auto height2 = otherEntity->getComponent<IntRectComponent>()->getIntRectWidth();
+                    if (position1.x + width1 > position2.x
+                    && position1.x < position2.x + width2
+                    && position1.y + height1 > position2.y
+                    && position1.y < position2.y + height2) {
+                        std::cout << position1.x << " " << position1.y << " " << width1 << " " << height1 << std::endl;
+                        std::cout << position2.x << " " << position2.y << " " << width2 << " " << height2 << std::endl;
+
                         std::cout << "intersection" << *entity << *otherEntity <<std::endl;
                     }
                 }
