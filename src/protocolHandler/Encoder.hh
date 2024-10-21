371e25d8d1337b28519b1c8e2208df93d07f00a8
Xavier ROGNON <xavier.rognon@epitech.eu>
2024-10-21 15:54:46 +0200

    [Fix] [~] = changing the heade in the packets to be more usable with the NACK

--- src/protocolHandler/Encoder.hh
+++ src/protocolHandler/Encoder.hh
@@ -15,7 +15,7 @@
 #include <array>
 
 #define U_STRING std::basic_string<unsigned char>
-#define PACKET_PER_TICK 64
+#define PACKET_MAX 256
 
 namespace RType {
 
@@ -45,39 +45,44 @@ namespace RType {
     */
     class Encoder {
     public:
+
+        /**
+        * @brief Add the number of the packet to the packet
+        *
+        * @param packet Packet to add the number
+        * @param number Number of the packet
+        *
+        * @return U_STRING Packet with the number
+        */
+        static U_STRING addPacketNumber(U_STRING &packet, std::uint8_t number);
+
         /**
         * @brief Encode the header of the packet
         *
-        * @param packetNumber The number of the packet
         * @param size Total size of the packet, excluding the header
         * @param type Type of the packet
         *
         * @return U_STRING Encoded header
         */
-        static U_STRING header(std::uint8_t packetNumber, std::uint16_t size, unsigned char type);
+        static U_STRING header(const std::uint16_t &size, const unsigned char &type);
 
         /**
         * @brief Encode the connexion packet
         *
-        * @param packetNumber The number of the packet
-        *
         * @return U_STRING Encoded connexion packet
         */
-        static U_STRING connexion(std::uint8_t packetNumber);
+        static U_STRING connexion();
 
         /**
         * @brief Encode the disconnexion packet
         *
-        * @param packetNumber The number of the packet
-        *
         * @return U_STRING Encoded disconnexion packet
         */
-        static U_STRING disconnexion(std::uint8_t packetNumber);
+        static U_STRING disconnexion();
 
         /**
         * @brief Encode the new entity packet
         *
-        * @param packetNumber The number of the packet
         * @param type Type of the entity
         * @param id The unique id of the entity
         * @param x The x position of the entity
@@ -85,24 +90,21 @@ namespace RType {
         *
         * @return U_STRING Encoded new entity packet
         */
-        static U_STRING newEntity(std::uint8_t packetNumber,
-                                  std::uint8_t type, std::uint16_t id,
-                                  std::uint16_t x, std::uint16_t y);
+        static U_STRING newEntity(const std::uint8_t &type, const std::uint16_t &id,
+                                  const std::uint16_t &x, const std::uint16_t &y);
 
         /**
         * @brief Encode the delete entity packet
         *
-        * @param packetNumber The number of the packet
         * @param id The unique id of the entity
         *
         * @return U_STRING Encoded delete entity packet
         */
-        static U_STRING deleteEntity(std::uint8_t packetNumber, std::uint16_t id);
+        static U_STRING deleteEntity(const std::uint16_t &id);
 
         /**
         * @brief Encode the entity movement packet
         *
-        * @param packetNumber The number of the packet
         * @param id The unique id of the entity
         * @param x The new x position of the entity
         * @param y The new y position of the entity
@@ -110,24 +112,22 @@ namespace RType {
         *
         * @return U_STRING Encoded move entity packet
         */
-        static U_STRING moveEntity(std::uint8_t packetNumber, std::uint16_t id,
-                                   std::uint16_t x, std::uint16_t y,
-                                   std::uint8_t rotation);
+        static U_STRING moveEntity(const std::uint16_t &id,
+                                   const std::uint16_t &x, const std::uint16_t &y,
+                                   const std::uint8_t &rotation);
 
         /**
         * @brief Encode the level information packet
         *
-        * @param packetNumber The number of the packet
         * @param level The level of the game
         *
         * @return U_STRING Encoded level information packet
         */
-        static U_STRING infoLevel(std::uint8_t packetNumber, std::uint8_t level);
+        static U_STRING infoLevel(const std::uint8_t &level);
 
         /**
         * @brief Encode the entity information packet
         *
-        * @param packetNumber The number of the packet
         * @param id The unique id of the entity
         * @param type The type of the entity
         * @param x The x position of the entity
@@ -137,68 +137,57 @@ namespace RType {
         *
         * @return U_STRING Encoded entity information packet
         */
-        static U_STRING infoEntity(std::uint8_t packetNumber, std::uint16_t id,
-                                   std::uint8_t type, std::uint16_t x,
-                                   std::uint16_t y, std::uint8_t rotation,
-                                   std::uint8_t life);
+        static U_STRING infoEntity(const std::uint16_t &id, const std::uint8_t &type,
+                                   const std::uint16_t &x, const std::uint16_t &y,
+                                   const std::uint8_t &rotation,
+                                   const std::uint8_t &life);
 
         /**
         * @brief Encode the player movement packet
         *
-        * @param packetNumber The number of the packet
         * @param x The inputs of the player in the x axis
         * @param y The inputs of the player in the y axis
         *
         * @return U_STRING Encoded player movement packet
         */
-        static U_STRING movePlayer(std::uint8_t packetNumber, std::int8_t x,
-                                   std::int8_t y);
+        static U_STRING movePlayer(const std::int8_t &x, const std::int8_t &y);
 
         /**
         * @brief Encode the player action packet
         *
-        * @param packetNumber The number of the packet
         * actions are not defined yet
         *
         * @return U_STRING Encoded player action packet
         */
-        static U_STRING actionPlayer(std::uint8_t packetNumber, bool action1,
-                                     bool action2, bool action3, bool action4);
+        static U_STRING actionPlayer(const bool &action1, const bool &action2,
+                                     const bool &action3, const bool &action4);
 
         /**
         * @brief Encode the game start packet
         *
-        * @param packetNumber The number of the packet
-        *
         * @return U_STRING Encoded game start packet
         */
-        static U_STRING gameStart(std::uint8_t packetNumber);
+        static U_STRING gameStart();
 
         /**
         * @brief Encode the game end packet
         *
-        * @param packetNumber The number of the packet
-        *
         * @return U_STRING Encoded game end packet
         */
-        static U_STRING gameEnd(std::uint8_t packetNumber);
+        static U_STRING gameEnd();
 
         /**
         * @brief Encode the error packet
         *
-        * @param packetNumber The number of the packet
-        *
         * @return U_STRING Encoded error packet
         */
-        static U_STRING error(std::uint8_t packetNumber);
+        static U_STRING error();
 
         /**
         * @brief Encode the ACK packet
         *
-        *
         * @return U_STRING Encoded ACK packet
         */
-        static U_STRING ACKMissing(std::uint8_t packetNumber,
-                                   std::vector<std::uint8_t> &packets);
+        static U_STRING ACKMissing(const std::vector<std::uint8_t> &packets);
     };
 }
