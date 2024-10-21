3e9d994f7c04dd856676626f42c4780df5376ae6
Xavier ROGNON <xavier.rognon@epitech.eu>
2024-10-21 18:54:24 +0200

    [Fix] [~] = updating the tests to be complient with the changes in the header

--- tests/protocolHandler/encoder_test.cpp
+++ tests/protocolHandler/encoder_test.cpp
@@ -16,24 +16,21 @@ Test(main, TestEncoderHeader) {
 
     testString += '\0';
     testString += '\0';
-    testString += 1;
-    outputString = RType::Encoder::header(1, 0, RType::CONNEXION);
+    outputString = RType::Encoder::header(0, RType::CONNEXION);
     cr_assert_eq(testString, outputString, "Expected: 0000000000000000, Got: %08b%08b",
                  outputString[0], outputString[1]);
 
     testString.clear();
     testString += 0b00001010;
-    testString += 0b10001000;
-    testString += 13;
-    outputString = RType::Encoder::header(13, 42, RType::ACTION_PLAYER);
+    testString += 0b10001100;
+    outputString = RType::Encoder::header(42, RType::ACTION_PLAYER);
     cr_assert_eq(testString, outputString, "Expected: 0000101010001000, Got: %08b%08b",
                  outputString[0], outputString[1]);
 
     testString.clear();
     testString += 0b00000101;
-    testString += 0b01000101;
-    testString += 32;
-    outputString = RType::Encoder::header(32, 21, RType::INFO_LEVEL);
+    testString += 0b01000110;
+    outputString = RType::Encoder::header(21, RType::INFO_LEVEL);
     cr_assert_eq(testString, outputString, "Expected: 0000101010001000, Got: %08b%08b",
                  outputString[0], outputString[1]);
 }
@@ -44,8 +41,7 @@ Test(main, TestConnexionPacket) {
 
     testString += '\0';
     testString += '\0';
-    testString += 12;
-    outputString = RType::Encoder::connexion(12);
+    outputString = RType::Encoder::connexion();
     cr_assert_eq(testString, outputString, "Expected: 0000000000000000, Got: %08b%08b",
                  outputString[0], outputString[1]);
 }
@@ -56,8 +52,7 @@ Test(main, TestDisconnexionPacket) {
 
     testString += '\0';
     testString += 1;
-    testString += 12;
-    outputString = RType::Encoder::disconnexion(12);
+    outputString = RType::Encoder::disconnexion();
     cr_assert_eq(testString, outputString, "Expected: 0000000000000001, Got: %08b%08b",
                  outputString[0], outputString[1]);
 }
